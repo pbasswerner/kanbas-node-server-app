811180c314cd3136c1790592868a83c2b8893d45
paula.bass.werner <paula.bass.werner@gmail.com>
2024-12-03 18:17:12 -0500

    Labs 5

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 import express from 'express';// import express from 'express'
 import Hello from './Hello.js';
 import Lab5 from './Lab5/index.js';
+import cors from 'cors';
 
 const app = express(); // create new express instance
+app.use(cors());
+app.use(express.json());
 Hello(app)
 Lab5(app)
-app.listen(process.env.PORT || 4000)// listen to http://localhost:4000
+app.listen(process.env.PORT || 4000)// listen to http://localhost:4
